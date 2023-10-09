89e76d75c75e6955bf295c825523078a83eae07e
Kent Coleman <coleman.a.kent@gmail.com>
2023-10-09 10:44:52 -0400

    First endpoint with json return function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello")
+from flask import *
+import json, time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET']) #End point with GET method
+def home_page():
+    data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
