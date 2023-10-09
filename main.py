01b4a6167ff94888c8d7bf5fe31288c7706cd63e
Kent Coleman <coleman.a.kent@gmail.com>
2023-10-09 10:58:31 -0400

    Added user request end point

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import json, time
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET']) #End point with GET method
+@app.route('/', methods=['GET']) # End point with GET method
 def home_page():
     data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+@app.route('/user/', methods=['GET']) # Request end point
+def request_page():
+    user_query = str(request.args.get('user')) # /user/?user=FIAHVSISDAH
+
+    data_set = {'Page': 'Request', 'Message': f'Successfully got the request for {user_query}', 'Timestamp': time.time()}
     json_dump = json.dumps(data_set)
 
     return json_dump
